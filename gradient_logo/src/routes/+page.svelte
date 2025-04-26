4e855ebba6e122bd2d12cac94fbaf17b7462e140
m-babii <maksymbabiiwork@gmail.com>
2025-04-26 17:33:08 +0300

    [gradient_logo] feat: gradient_logo DONE!

--- gradient_logo/src/routes/+page.svelte
+++ gradient_logo/src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import logo from '@/assets/images/logo.png';
+
+	import type { Action } from 'svelte/action';
+
+	const cardAction: Action = (node) => {
+		if (!node) return;
+
+		function handleMouseMove(event: MouseEvent) {
+			requestAnimationFrame(() => {
+				if (!node) return;
+				const cardRect = node.getBoundingClientRect();
+				const cardX = cardRect.left + cardRect.width / 2;
+				const cardY = cardRect.top + cardRect.height / 2;
+				const angleX = (event.clientY - cardY) / 10;
+				const angleY = (event.clientX - cardX) / -10;
+
+				node.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+				node.style.boxShadow = `${angleY * 2}px ${angleX * 2}px 30px rgba(0, 0, 0, 0.3)`;
+			});
+		}
+
+		function handleMouseLeave() {
+			if (!node) return;
+			node.style.transform = `rotateX(0deg) rotateY(0deg)`;
+			node.style.boxShadow = `0 10px 20px rgba(0, 0, 0, 0.1)`;
+		}
+
+		$effect(() => {
+			if (!node) return;
+			node.addEventListener('mousemove', handleMouseMove);
+			node.addEventListener('mouseleave', handleMouseLeave);
+
+			return () => {
+				node.removeEventListener('mousemove', handleMouseMove);
+				node.removeEventListener('mouseleave', handleMouseLeave);
+			};
+		});
+	};
+</script>
+
+<section class="h-full">
+	<div class="perspective-distant flex size-full flex-col items-center justify-center">
+		<div
+			use:cardAction
+			class="shadow-card size-110 rounded-140 transform-3d relative flex flex-col items-center justify-center overflow-hidden bg-white px-14 py-20 text-center transition-all duration-100"
+		>
+			<div class="translate-z-5 relative flex flex-col items-center justify-center gap-10 overflow-hidden">
+				<div class="shrink-1 flex items-center justify-center overflow-hidden">
+					<img src={logo} alt="Bing" class="size-full object-contain" />
+				</div>
+				<h2 class="text-primary shrink-0 grow text-4xl font-light">Bing</h2>
+			</div>
+		</div>
+	</div>
+</section>
--- gradient_logo/svelte.config.js
+++ gradient_logo/svelte.config.js
@@ -1,12 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const IGNORE_WARNINGS = [
+	'a11y_click_events_have_key_events',
+	'a11y_no_noninteractive_element_interactions'
+];
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	compilerOptions: {
+		warningFilter(warning) {
+			return !IGNORE_WARNINGS.includes(warning.code);
+		}
+
+	},
+	
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
